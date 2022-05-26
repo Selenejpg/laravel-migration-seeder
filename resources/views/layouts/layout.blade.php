25a59624e66a24db4da2dbc6b584d008da392371
noemipintaldi <noemipintaldi@gmail.com>
2022-05-26 16:34:01 +0200

    navbar aggiunta

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
-    <title>Document</title>
+    <title>Layout</title>
 </head>
 <body>
     
-
+    @include('includes.header')
 
     <script src=" {{ asset('js/app.js') }} "></script>
 </body>
